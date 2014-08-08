0bac7d752296f7969060a070b550f7b942a35e15
Jethro Muller <JethroMuller@gmail.com>
2014-08-08 23:36:32 +0200

    Format code and indentations. Reorder methods.

--- core/src/com/mygdx/tetris/TetrisBlock.java
+++ core/src/com/mygdx/tetris/TetrisBlock.java
@@ -1,19 +1,22 @@
 package com.mygdx.tetris;
 
 public class TetrisBlock {
-    private static final int[][][][] blockStates = {
-            {{{-1, 0}, {1, 0}, {2, 0}}, {{0, -1}, {0, 1}, {0, 2}}}, //i //0
-            {{{0, -1}, {0, 1}, {-1, 1}}, {{-1, 0}, {1, 0}, {1, 1}}, {{1, -1}, {0, -1}, {0, 1}}, {{-1, -1}, {-1, 0}, {1, 0}}}, //l //1
-            {{{0, -1}, {0, 1}, {1, 1}}, {{1, -1}, {1, 0}, {-1, 0}}, {{-1, -1}, {0, -1}, {0, 1}}, {{-1, 0}, {1, 0}, {-1, 1}}}, //j //2
-            {{{0, 1}, {1, 0}, {1, 1}}}, //o //3
-            {{{0, 1}, {-1, 1}, {1, 0}}, {{0, -1}, {1, 0}, {1, 1}}}, //s //4
-            {{{-1, 0}, {0, 1}, {1, 1}}, {{1, -1}, {1, 0}, {0, 1}}}, //z //5
-            {{{-1, 0}, {1, 0}, {0, 1}}, {{0, -1}, {1, 0}, {0, 1}}, {{-1, 0}, {1, 0}, {0, -1}}, {{0, -1}, {-1, 0}, {0, 1}}},  //t //6
-    };
-    private int[][] blockArray;
+    private static final int[][][][] blockStates =
+            {{{{- 1, 0}, {1, 0}, {2, 0}}, {{0, - 1}, {0, 1}, {0, 2}}}, //i //0
+             {{{0, - 1}, {0, 1}, {- 1, 1}}, {{- 1, 0}, {1, 0}, {1, 1}},
+              {{1, - 1}, {0, - 1}, {0, 1}}, {{- 1, - 1}, {- 1, 0}, {1, 0}}}, //l //1
+             {{{0, - 1}, {0, 1}, {1, 1}}, {{1, - 1}, {1, 0}, {- 1, 0}},
+              {{- 1, - 1}, {0, - 1}, {0, 1}}, {{- 1, 0}, {1, 0}, {- 1, 1}}}, //j //2
+             {{{0, 1}, {1, 0}, {1, 1}}}, //o //3
+             {{{0, 1}, {- 1, 1}, {1, 0}}, {{0, - 1}, {1, 0}, {1, 1}}}, //s //4
+             {{{- 1, 0}, {0, 1}, {1, 1}}, {{1, - 1}, {1, 0}, {0, 1}}}, //z //5
+             {{{- 1, 0}, {1, 0}, {0, 1}}, {{0, - 1}, {1, 0}, {0, 1}}, {{- 1, 0}, {1, 0}, {0, - 1}},
+              {{0, - 1}, {- 1, 0}, {0, 1}}},  //t //6
+            };
     protected int blockType;
-    private int[] center;
     protected int rotationState = 0;
+    private int[][] blockArray;
+    private int[] center;
     private boolean placed = false;
 
 
@@ -21,6 +24,20 @@ public class TetrisBlock {
         blockArray = blockStates[blockType][rotationState].clone();
         this.blockType = blockType;
         center = new int[] {2, 4};
+        updateBlockArray();
+    }
+
+    public void updateBlockArray() {
+        for (int i = 0; i < blockArray.length; i++) {
+            blockArray[i] = new int[] {center[0] + blockStates[blockType][rotationState][i][0],
+                                       center[1] + blockStates[blockType][rotationState][i][1]};
+        }
+    }
+
+    public TetrisBlock(int blockType, int[] center, int rotationState) {
+        blockArray = blockStates[blockType][rotationState].clone();
+        this.blockType = blockType;
+        this.center = center.clone();
         updateBlockArray();
     }
 
@@ -32,27 +49,13 @@ public class TetrisBlock {
         return placed;
     }
 
-    public TetrisBlock(int blockType, int[] center, int rotationState) {
-        blockArray = blockStates[blockType][rotationState].clone();
-        this.blockType = blockType;
-        this.center = center.clone();
-        updateBlockArray();
-    }
-
-    public void updateBlockArray() {
-        for (int i = 0; i < blockArray.length; i++) {
-            blockArray[i] = new int[] {center[0] + blockStates[blockType][rotationState][i][0],
-                                       center[1] + blockStates[blockType][rotationState][i][1]};
-        }
-    }
-
     public int[][] getNextRotation() {
         int tempRotation = (rotationState + 1) % blockStates[blockType].length;
         int[][] temp = blockStates[blockType][tempRotation].clone();
         int[][] tempReturn = new int[temp.length][2];
         for (int i = 0; i < temp.length; i++) {
             tempReturn[i] = new int[] {center[0] + blockStates[blockType][tempRotation][i][0],
-                    center[1] + blockStates[blockType][tempRotation][i][1]};
+                                       center[1] + blockStates[blockType][tempRotation][i][1]};
         }
         return tempReturn.clone();
     }
@@ -74,12 +77,12 @@ public class TetrisBlock {
         String blockString = "";
         String blockGrid[][] = new String[4][4];
         blockGrid[1][1] = "[]";
-        for (int[] blockCoords: blockStates[blockType][rotationState]) {
+        for (int[] blockCoords : blockStates[blockType][rotationState]) {
             blockGrid[1 + blockCoords[0]][1 + blockCoords[1]] = "[]";
         }
 
-        for (String[] blockRow: blockGrid) {
-            for (String block: blockRow) {
+        for (String[] blockRow : blockGrid) {
+            for (String block : blockRow) {
                 if (block != null) {
                     blockString += block;
                 } else {
